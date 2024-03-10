bf0e6080eb71f4286f66eaadfe9dab5304fc5baf
Michael <104609759+mollmikey@users.noreply.github.com>
2024-03-11 11:21:56 +1300

    Respond with string, Respond with JSON

--- server/server.ts
+++ server/server.ts
@@ -2,4 +2,11 @@ import express from 'express'
 
 const server = express()
 
+server.get('/compliment', (req, res) => {
+  res.json({
+    category: 'fashion',
+    powerLevel: 10,
+    text: 'What a fancy blazer~',
+  })
+})
 export default server
