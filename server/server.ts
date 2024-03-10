8a28eba18284f970e7c5ebe1ebae96c95020551f
Sam <121822122+samuel-tofatimoteo@users.noreply.github.com>
2024-03-11 12:38:17 +1300

    Finished steps 3 and 4

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as Path from 'node:path'
 
 const server = express()
 
@@ -8,5 +9,21 @@ server.get('/compliment', (req, res) => {
     powerLevel: 10,
     text: 'What a fancy blazer~',
   })
+})
+
+// server.get('/secrets', (req, res) => {
+//   res.sendFile(Path.resolve('./server/secrets.json'))
+// })
+
+server.get('/secrets', (req, res) => {
+  const secretType = req.query.type
+
+  if (secretType === 'mystical') {
+    res.sendFile(Path.resolve('./server/mystical-secrets.json'))
+  } else if (secretType === 'kitchen') {
+    res.sendFile(Path.resolve('./server/kitchen-secrets.json'))
+  } else {
+    res.sendFile(Path.resolve('./server/secrets.json'))
+  }
 })
 export default server
