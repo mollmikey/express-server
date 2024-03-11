cc479b3d9301e76579603a43057351be843cb687
Sam <121822122+samuel-tofatimoteo@users.noreply.github.com>
2024-03-11 13:52:57 +1300

    5-Respond based on a url parameter attempted, else if works

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,10 @@
 import express from 'express'
-import * as Path from 'node:path'
+import Path from 'node:path'
+import url from 'node:url'
 
 const server = express()
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = Path.dirname(__filename)
 
 server.get('/compliment', (req, res) => {
   res.json({
@@ -18,10 +21,28 @@ server.get('/secrets', (req, res) => {
     res.sendFile(Path.resolve('./server/mystical-secrets.json'))
   } else if (secretType === 'kitchen') {
     res.sendFile(Path.resolve('./server/kitchen-secrets.json'))
-  } else if (secretType === 'lizards') {
-    res.sendStatus(404)
+    // } else if (secretType === 'lizards') {
+    //   res.sendStatus(404)
   } else {
     res.sendFile(Path.resolve('./server/secrets.json'))
+  }
+})
+
+// server.get('/secrets/:type', (req, res) => {
+//   let type = req.params.type
+//   if (type == 'kitchen' || type == 'mystical') {
+//     res.sendFile(Path.join(__dirname, `/${type}-secrets.json`))
+//   } else {
+//     res.sendStatus(404)
+//   }
+// })
+
+server.get('/secrets/:type', (req, res) => {
+  let type = req.params.type
+  if (type == 'mystical' || type == 'kitchen') {
+    res.sendFile(Path.join(__dirname, `/${type}-secrets.json`))
+  } else {
+    res.sendStatus(404)
   }
 })
 
